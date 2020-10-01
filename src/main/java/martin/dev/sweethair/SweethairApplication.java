bb3298ebaf64a5b554b6fa01e8259c436e26cf5b
Martin <mselvenis@gmail.com>
2020-10-01 20:13:33 +0100

    initial commit

---
+++ src/main/java/martin/dev/sweethair/SweethairApplication.java
@@ -0,0 +1,13 @@
+package martin.dev.sweethair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweethairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweethairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/martin/dev/sweethair/SweethairApplicationTests.java
@@ -0,0 +1,13 @@
+package martin.dev.sweethair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweethairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

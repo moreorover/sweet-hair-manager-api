98a7e396026b3969523444487c4c67d3d432b978
Martin <mselvenis@gmail.com>
2020-10-22 18:53:48 +0100

    insert appointments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ INSERT INTO users (first_name, last_name) VALUES ('fName2', 'sName2');
 INSERT INTO users (first_name, last_name) VALUES ('fName3', 'sName3');
 INSERT INTO users (first_name, last_name) VALUES ('fName4', 'sName4');
 
-INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:21', '11:43:22', 1, 'Hair extensions.');
-INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:21', '11:43:22', 2, 'Hair extensions.');
-INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:21', '11:43:22', 3, 'Hair extensions.');
+INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:00', '11:43:00', 1, 'Hair extensions.');
+INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:00', '11:43:00', 2, 'Hair extensions.');
+INSERT INTO appointments (date, start_time, finish_time, user_id, description) VALUES ('2020-10-14', '11:43:00', '11:43:00', 3, 'Hair extensions.');
